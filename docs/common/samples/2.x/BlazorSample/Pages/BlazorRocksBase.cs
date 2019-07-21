e39f0e11f3ccdf5de4488573ba8decfa7e3b9410
Wiesiek <wiesiek.romanowski@gmail.com>
2019-07-21 09:53:13 +0200

    Commit

--- docs/common/samples/2.x/BlazorSample/Pages/BlazorRocksBase.cs
+++ docs/common/samples/2.x/BlazorSample/Pages/BlazorRocksBase.cs
@@ -3,4 +3,9 @@ using Microsoft.AspNetCore.Blazor.Components;
 public class BlazorRocksBase : BlazorComponent
 {
     public string BlazorRocksText { get; set; } = "Blazor rocks the browser!";
+
+    public string ReturnString(string text)
+    {
+        return text;
+    }
 }
